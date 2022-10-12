f7a9a603e42fcd6ad8cecc4a33e9505467dafbdf
Sai Koukuntla <sai.koukunt@gmail.com>
2022-10-12 02:28:38 -0400

    ported PoissonGP code

--- burst_detector/__init__.py
+++ burst_detector/__init__.py
@@ -1,2 +1,4 @@
 from burst_detector.utils import find_times, clust_counts, fix_clust_ids
 from burst_detector.overlap import overlap, overlap_norm
+from burst_detector.mk_GP_mat import mk_GP_mat
+from burst_detector.fit_PoissonGP import fit_PoissonGP
--- burst_detector/mk_GP_mat.py
+++ burst_detector/mk_GP_mat.py
@@ -1,16 +1,16 @@
 import numpy as np
 import warnings
 
-def mk_GP_mat(T1, T2, params)
+def mk_GP_mat(T1, T2, params):
 
     # Input parsing
     if len(params) == 1:
-        params = np.array([1, params, 2])
+        params = np.array([1, params[0], 2])
     elif len(params) == 2:
         params = np.array([params[0], params[1], 2])
     elif len(params) == 3:
         params = np.array(params)
-    else
+    else:
         warnings.warn("params has too many elements. Using the first 3 only!")
         params = np.array(params[:3])
 
@@ -18,7 +18,7 @@ def mk_GP_mat(T1, T2, params)
     T1 = T1[..., np.newaxis]
     T2 = T2[..., np.newaxis]
 
-    K = np.abs(np.tile(T1,(T1.T.shape)) - np.tile(T2.T,(T2.shape)))
+    K = np.abs(np.tile(T1,(T2.T.shape)) - np.tile(T2.T,(T1.shape)))
     K = params[0]*np.exp(-(K**params[2])/(params[1]**params[2]))
 
-return K
+    return K
