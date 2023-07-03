fixes for normalization, merging logic

burst_detector/algo.py
@@ -8,13 +8,14 @@ import time
 
 def run_merge(params):
     
-    # load files
+    # load spike time and cluster estimates
     print("Loading files...")
     
     times = np.load(os.path.join(params['KS_folder'], 'spike_times.npy')).flatten()
     clusters = np.load(os.path.join(params['KS_folder'], 'spike_clusters.npy')).flatten() 
     n_clust = clusters.max() + 1
     
+    # load mean_wf if it exists
     if not params['calc_means']:
         try:
             mean_wf = np.load(os.path.join(params['KS_folder'], 'mean_waveforms.npy'))
@@ -25,22 +26,22 @@ def run_merge(params):
     else:
         mean_wf = None
             
+    # count spikes per cluster, load quality labels
     counts = bd.spikes_per_cluster(clusters)
     labels = pd.read_csv(os.path.join(params['KS_folder'], 'cluster_group.tsv'), sep="\t")
         
-    # load data
+    # load recording
     rawData = np.memmap(params['data_filepath'], dtype='int16', mode='r')
     data = np.reshape(rawData, (int(rawData.size/params['n_chan']), params['n_chan']))
-    # data = None
-    
-    
+
     t0 = time.time()
     print("Loaded data, calculating mean similarity...")
     
+    # group spike times by cluster identity
     times_multi = bd.find_times_multi(times, clusters, np.arange(clusters.max()+1))
     
     # calculate mean similarity
-    mean_sim, offset, wf_means_norm, mean_wf, pass_ms = bd.stages.calc_mean_sim(
+    mean_sim, offset, wf_norms, mean_wf, pass_ms = bd.stages.calc_mean_sim(
         data, 
         times_multi, 
         clusters, 
@@ -54,57 +55,56 @@ def run_merge(params):
     mean_sim_time = time.strftime('%H:%M:%S', time.gmtime(t1-t0))
     print("Found %d candidate cluster pairs" % (pass_ms.sum()/2))
     
-    if not params['skip_cross_sim']:
-        print("Pre-caching spikes for cross-projection...")
-        # cache spikes and free data RAM
-        spikes = {}
-        num = 0
-        for c1 in range(n_clust):
-            for c2 in range(c1+1, n_clust):
-                if pass_ms[c1,c2]:
-                    num += 1
-                    # if (num % 50 == 0):
-                    #     print(num)
-
-                    if c1 not in spikes:
-                        spikes[c1] = bd.extract_spikes(
-                            data, 
-                            times_multi, 
-                            clusters, 
-                            c1, 
-                            pre_samples=params['pre_samples'],
-                            post_samples=params['post_samples'],
-                            n_chan=params['n_chan'],
-                            max_spikes=params['max_spikes']
-                        )
-                    if c2 not in spikes:
-                        spikes[c2] = bd.extract_spikes(
-                            data, 
-                            times_multi, 
-                            clusters, 
-                            c2, 
-                            pre_samples=params['pre_samples'],
-                            post_samples=params['post_samples'],
-                            n_chan=params['n_chan'],
-                            max_spikes=params['max_spikes']
-                        )
-        data = None
-
-        t2 = time.time()
-        cache_spikes_time = time.strftime('%H:%M:%S', time.gmtime(t2-t1))
-        print("Caching spikes took %s" % cache_spikes_time)
-
-        print("Calculating cross-projection similarity...")
-        # calculate cross-projection similarity and free spikes RAM
-        cross_sim = bd.stages.calc_cross_sim(spikes, wf_means_norm,\
-                                             offset, pass_ms, n_clust)
-        spikes = None
-        t3 = time.time()
-        cross_sim_time = time.strftime('%H:%M:%S', time.gmtime(t3-t2))
-        print("Cross projection took %s" % cross_sim_time)
+    mean_sim[pass_ms == False] = 0
     
-    print("Calculating cross-correlation metric...")
+#     if not params['skip_cross_sim']:
+#         print("Pre-fetching spikes for cross-projection...")
+#         # pre-fetch spikes and free data RAM
+#         spikes = {}
+#         num = 0
+#         for c1 in range(n_clust):
+#             for c2 in range(c1+1, n_clust):
+#                 if pass_ms[c1,c2]:
+#                     num += 1
+
+#                     if c1 not in spikes:
+#                         spikes[c1] = bd.extract_spikes(
+#                             data, 
+#                             times_multi, 
+#                             clusters, 
+#                             c1, 
+#                             pre_samples=params['pre_samples'],
+#                             post_samples=params['post_samples'],
+#                             n_chan=params['n_chan'],
+#                             max_spikes=params['max_spikes']
+#                         )
+#                     if c2 not in spikes:
+#                         spikes[c2] = bd.extract_spikes(
+#                             data, 
+#                             times_multi, 
+#                             clusters, 
+#                             c2, 
+#                             pre_samples=params['pre_samples'],
+#                             post_samples=params['post_samples'],
+#                             n_chan=params['n_chan'],
+#                             max_spikes=params['max_spikes']
+#                         )
+#         data = None
+
+#         t2 = time.time()
+#         cache_spikes_time = time.strftime('%H:%M:%S', time.gmtime(t2-t1))
+#         print("Caching spikes took %s" % cache_spikes_time)
+#         print("Calculating cross-projection similarity...")
+        
+#         # calculate cross-projection similarity and free spikes RAM
+#         cross_sim = bd.stages.calc_cross_sim(spikes, offset, mean_wf, wf_norms, pass_ms, n_clust)
+#         spikes = None
+#         t3 = time.time()
+#         cross_sim_time = time.strftime('%H:%M:%S', time.gmtime(t3-t2))
+#         print("Cross projection took %s" % cross_sim_time)
+    
     # calculate xcorr metric
+    print("Calculating cross-correlation metric...")
     xcorr_sig, xgrams, shfl_xgrams = bd.stages.calc_xcorr_metric(
         times, 
         clusters,
@@ -113,24 +113,24 @@ def run_merge(params):
         params
     )
     t4 = time.time()
-    xcorr_time = time.strftime('%H:%M:%S', time.gmtime(t4-t3))
+    xcorr_time = time.strftime('%H:%M:%S', time.gmtime(t4-t1))
     print("Cross correlation took %s" % xcorr_time)
     
-    print("Calculating refractory period penalty...")
     # calculate refractory penalty
+    print("Calculating refractory period penalty...")
     ref_pen, ref_per = bd.stages.calc_ref_p(times, clusters, n_clust, pass_ms, xcorr_sig,\
                                    params)
     t5 = time.time()
     ref_pen_time = time.strftime('%H:%M:%S', time.gmtime(t5-t4))
     print("Refractory period penalty took %s" % ref_pen_time)
-    
-    print("Calculating final metric...")
+
     # calculate final metric
+    print("Calculating final metric...")
     final_metric = np.zeros_like(mean_sim)
     
     for c1 in range(n_clust):
         for c2 in range(c1, n_clust):
-            met = np.sqrt(cross_sim[c1,c2] * cross_sim[c2,c1]) + \
+            met = mean_sim[c1,c2] + \
             params['xcorr_coeff']*xcorr_sig[c1,c2] - \
             params['ref_pen_coeff']*ref_pen[c1,c2]
             
@@ -142,9 +142,8 @@ def run_merge(params):
     
     channel_map = np.load(os.path.join(params['KS_folder'], 'channel_map.npy')).flatten()
     
-    clusters, co, merge_lists, stage_mets = bd.stages.merge_clusters(
-        times, clusters, counts, n_clust, final_metric, cross_sim, xcorr_sig, 
-        ref_pen, mean_wf, channel_map, params)
+    old2new, new2old = bd.stages.merge_clusters(
+        clusters, counts, mean_wf, final_metric, params)
    
     t6 = time.time()
     merge_time = time.strftime('%H:%M:%S', time.gmtime(t6-t5))
@@ -154,87 +153,57 @@ def run_merge(params):
     print("Writing to output...")
     # write spike table output
     # weighted avg new waveforms
-    new2old = merge_lists[1]
+#     new2old = merge_lists[1]
 
-    old_n = mean_wf.shape[0]
-    n = clusters.max() + 1
-    temp = np.zeros((n, mean_wf.shape[1], mean_wf.shape[2]))
-    temp[:old_n,:,:] = mean_wf
-    mean_wf = temp
+#     old_n = mean_wf.shape[0]
+#     n = clusters.max() + 1
+#     temp = np.zeros((n, mean_wf.shape[1], mean_wf.shape[2]))
+#     temp[:old_n,:,:] = mean_wf
+#     mean_wf = temp
     
-    # convert counts to array
-    temp = np.zeros(n_clust)
-    for i in range(n_clust):
-        if i in counts:
-            temp[i] = counts[i]
-    counts = temp
+#     # convert counts to array
+#     temp = np.zeros(n_clust)
+#     for i in range(n_clust):
+#         if i in counts:
+#             temp[i] = counts[i]
+#     counts = temp
     
-    temp = np.zeros(n)
-    temp[:old_n] = counts
-    counts = temp
+#     temp = np.zeros(n)
+#     temp[:old_n] = counts
+#     counts = temp
 
-    # calculate mean waveforms for new arrays
-    for i in range(old_n, n):
-        c1 = new2old[i][0]; c2 = new2old[i][1]
-        mean_wf[i] = (counts[c1]/(counts[c1]+counts[c2])) * mean_wf[c1] + \
-        (counts[c2]/(counts[c1]+counts[c2])) * mean_wf[c2]
-        counts[i] = counts[c1] + counts[c2]
-    
-    # fix old2new and new2old
-    old2new = merge_lists[0]
-    new2old = merge_lists[1]
-    
-    for key, value in old2new.items():
-        if value in old2new.keys():
-            while value in old2new.keys():
-                value = old2new[value]
-            old2new[key] = value
-
-    trash = []
-    for key in old2new.keys():
-        if int(key) >= old_n:
-            trash.append(key)
-    for key in trash:
-        del old2new[key]
-        
-    new2old = {}
-    for key, value in old2new.items():
-        if value in new2old.keys():
-            new2old[value].append(int(key))
-        else:
-            new2old[value] = [int(key)]
             
-    # calculate peak channels 
-    mean_wf = mean_wf[:, channel_map, :]
-    peak_chans = np.argmax(np.max(mean_wf, 2) - np.min(mean_wf,2),1)
-    channel_pos = np.load(os.path.join(params['KS_folder'], 'channel_positions.npy'))
+#     # calculate peak channels 
+#     mean_wf = mean_wf[:, channel_map, :]
+#     peak_chans = np.argmax(np.max(mean_wf, 2) - np.min(mean_wf,2),1)
+#     channel_pos = np.load(os.path.join(params['KS_folder'], 'channel_positions.npy'))
 
-    # construct spike table dataframe
-    X = channel_pos[peak_chans[clusters],0]
-    Y = channel_pos[peak_chans[clusters],1]
+#     # construct spike table dataframe
+#     X = channel_pos[peak_chans[clusters],0]
+#     Y = channel_pos[peak_chans[clusters],1]
     
-    spike_table = pd.DataFrame(
-        {"time": times,
-        "cluster": clusters,
-        "X": X,
-        "Y": Y}
-    )
+#     spike_table = pd.DataFrame(
+#         {"time": times,
+#         "cluster": clusters,
+#         "X": X,
+#         "Y": Y}
+    # )
 
     os.makedirs(os.path.join(params['KS_folder'], "automerge"), exist_ok=True)
-    spike_table.to_csv(os.path.join(params['KS_folder'], "automerge", "spike_table.csv"), header=False, index=False)
-    np.save(os.path.join(params['KS_folder'], "automerge", "clusters.npy"), clusters)
-    np.save(os.path.join(params['KS_folder'], "automerge", "cross_sim.npy"), stage_mets[0])
-    np.save(os.path.join(params['KS_folder'], "automerge", "xcorr_sig.npy"), stage_mets[1])
-    np.save(os.path.join(params['KS_folder'], "automerge", "ref_pen.npy"), stage_mets[2])
+    # spike_table.to_csv(os.path.join(params['KS_folder'], "automerge", "spike_table.csv"), header=False, index=False)
+    # np.save(os.path.join(params['KS_folder'], "automerge", "clusters.npy"), clusters)
+    # np.save(os.path.join(params['KS_folder'], "automerge", "cross_sim.npy"), stage_mets[0])
+    # np.save(os.path.join(params['KS_folder'], "automerge", "xcorr_sig.npy"), stage_mets[1])
+    # np.save(os.path.join(params['KS_folder'], "automerge", "ref_pen.npy"), stage_mets[2])
 
     with open(os.path.join(params['KS_folder'], "automerge", "old2new.json"), "w") as file:
         file.write(json.dumps(old2new))
 
     with open(os.path.join(params['KS_folder'], "automerge", "new2old.json"), "w") as file:
         file.write(json.dumps(new2old))
-    
-    with open(os.path.join(params['KS_folder'], "automerge", "rejected.json"), "w") as file:
-        file.write(json.dumps(merge_lists[2]))
+        
+    merges = list(new2old.values())
+    bd.plot.plot_merges(merges, times_multi, mean_wf, params)
     
     t7 = time.time()
     total_time = time.strftime('%H:%M:%S', time.gmtime(t7-t0))
@@ -242,4 +211,4 @@ def run_merge(params):
     print("Total time: %s" % total_time)
     
     
-    return mean_sim_time, cache_spikes_time, cross_sim_time, xcorr_time, ref_pen_time, merge_time, total_time, int(clusters.max() + 1 - old_n), int(old_n)
+    return mean_sim_time, xcorr_time, ref_pen_time, merge_time, total_time, len(list(new2old.keys())), int(clusters.max())

burst_detector/cluster_metrics.py
@@ -3,9 +3,9 @@ import burst_detector as bd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def normalize_wfs(wfs):
+def calc_wf_norms(wfs):
     """
-    Normalizes a each waveform in an array of waveforms with respect to the Frobenius norm.
+    Calculates the Frobenius norm of each waveform in an array of waveforms.
     
     Parameters
     ----------
@@ -16,23 +16,19 @@ def normalize_wfs(wfs):
         
     Returns
     -------
-    wfs_norm: array-like
+    wf_norm: array-like
         Array of waveforms where each waveform has been normalized to have a Frobenius norm
         of 1. Has same shape as input
     """
-    n_wf = wfs.shape[0]
+    wf_norms = np.zeros(wfs.shape[0])
     
-    wfs_norm = wfs.copy()
-    
-    for i in range(n_wf):
-        norm = np.linalg.norm(wfs[i])
-        if norm > 0:
-            wfs_norm[i] /= norm
+    for i in range(wfs.shape[0]):
+        wf_norms[i] = np.linalg.norm(wfs[i])
             
-    return wfs_norm
+    return wf_norms
 
 
-def wf_means_similarity(mean_wf, jitter=False, jitter_amt = 4):
+def wf_means_similarity(mean_wf, jitter=False, jitter_amt=4):
     """
     Calculates the normalized pairwise similarity (inner product) between every pair of 
     waveforms in an array of waveforms.
@@ -61,25 +57,45 @@ def wf_means_similarity(mean_wf, jitter=False, jitter_amt = 4):
         Array containing normalized input waveforms, where each waveform is normalized with
         respect to its Frobenius norm.
     """
+#     n_clust = mean_wf.shape[0]
+#     mean_sim = np.zeros((n_clust, n_clust))
+#     offsets = np.zeros((n_clust, n_clust), dtype='int16')
+#     mean_wf_norm = normalize_wfs(mean_wf)
+        
+#     for i in range(n_clust):
+#         for j in range(n_clust):
+#             if i != j:
+#                 if jitter:
+#                     sim, off = sim_jitter(mean_wf_norm[i], mean_wf_norm[j], jitter_amt)
+#                     mean_sim[i, j] = sim
+#                     offsets[i,j] = off
+#                 else:
+#                     mean_sim[i,j] = np.dot(mean_wf_norm[i].flatten(), mean_wf_norm[j].flatten())
+        
+#     return mean_sim, offsets, mean_wf_norm
+
     n_clust = mean_wf.shape[0]
     mean_sim = np.zeros((n_clust, n_clust))
     offsets = np.zeros((n_clust, n_clust), dtype='int16')
-    mean_wf_norm = normalize_wfs(mean_wf)
-        
+    wf_norms = calc_wf_norms(mean_wf)
+    
     for i in range(n_clust):
         for j in range(n_clust):
             if i != j:
+                norm = max(wf_norms[i], wf_norms[j])
+                if norm == 0:
+                    continue
+                
                 if jitter:
-                    sim, off = sim_jitter(mean_wf_norm[i], mean_wf_norm[j], jitter_amt)
-                    mean_sim[i, j] = sim
+                    sim, off = sim_jitter(mean_wf[i], mean_wf[j], norm, jitter_amt)
+                    mean_sim[i, j] = sim/(norm**2)
                     offsets[i,j] = off
                 else:
-                    mean_sim[i,j] = np.dot(mean_wf_norm[i].flatten(), mean_wf_norm[j].flatten())
-        
-    return mean_sim, offsets, mean_wf_norm
+                    mean_sim[i, j] = np.dot(mean_wf[i].flatten(), mean_wf[j].flatten())/(norm**2)
+                    
+    return mean_sim, wf_norms, offsets
 
-
-def sim_jitter(m1, m2, jitter_amt):
+def sim_jitter(m1, m2, norm, jitter_amt):
     """
     Calculates the maximum inner product between two waveforms with respect to a time shift.
     
@@ -87,6 +103,8 @@ def sim_jitter(m1, m2, jitter_amt):
     ----------
     m1, m2: array-like
         2-D input waveforms of shape (# of channels, # of timepoints)
+    norm: float
+        max of the 2-norms of the input waveforms
     jitter_amt: int
         Number of samples to time-shift in each direction.
         
@@ -116,14 +134,14 @@ def sim_jitter(m1, m2, jitter_amt):
             mean_sim = off_sim
             offset = i
 
-        if mean_sim - np.dot(m1.flatten(),m2.flatten()) < .1:
+        if (mean_sim - np.dot(m1.flatten(),m2.flatten())) < .1:
             mean_sim = np.dot(m1.flatten(),m2.flatten())
             offset = 0
             
     return mean_sim, offset
 
 
-def cross_proj(c1_spikes, c2_spikes, c1_mean_norm, c2_mean_norm, offset=0, norm=True):
+def cross_proj(c1_spikes, c2_spikes, c1_mean, c2_mean, c1_norm, c2_norm, offset=0):
     """
     Returns the cross-projections of spikes onto normalized mean waveforms for a 
     pair of clusters.
@@ -133,40 +151,34 @@ def cross_proj(c1_spikes, c2_spikes, c1_mean_norm, c2_mean_norm, offset=0, norm=
     c1_spikes, c2_spikes: array-like
         An array of spikes in the specified cluster with shape (# of spikes, # of channels, 
         # of timepoints).
-    c1_mean_norm, c2_mean_norm: array_like
-        The normalized mean waveform for the respective cluster. Should have shape (# of channels, # of 
+    c1_mean, c2_mean: array_like
+        The mean waveform for the respective cluster. Should have shape (# of channels, # of 
         timepoints)
+    c1_norm, c2_norm : array_like
+       Frobenius norms for the mean waveform of the respective cluster
     offset: int
         Offset to time-shift spikes and means by before projection.
-    norm: boolean, optional
-        If True, spike waveforms are normalized with respect to their Frobenius norms
-        before projection. True by default.
         
     Returns
     -------
     proj_1on1, proj_2on1, proj_1on2, proj_2on2: array-like
         1-D arrays containing the projections of spikes onto mean waveforms. 
     """    
-    
-    if norm:
-        c1_spikes = normalize_wfs(c1_spikes)
-        c2_spikes = normalize_wfs(c2_spikes)
-        
     t_length = c1_spikes.shape[2]
     
     # adjust for offset
     if offset < 0:
         c1_spikes = c1_spikes[:,:,:t_length+offset]
-        c1_mean_norm = c1_mean_norm[:,:t_length+offset]
+        c1_mean = c1_mean[:,:t_length+offset]
         
         c2_spikes = c2_spikes[:,:,offset*-1:]
-        c2_mean_norm = c2_mean_norm[:,offset*-1:]
+        c2_mean = c2_mean[:,offset*-1:]
     else:
         c1_spikes = c1_spikes[:,:,offset:]
-        c1_mean_norm = c1_mean_norm[:,offset:]
+        c1_mean = c1_mean[:,offset:]
     
         c2_spikes = c2_spikes[:,:,:t_length-offset]
-        c2_mean_norm = c2_mean_norm[:,:t_length-offset]
+        c2_mean = c2_mean[:,:t_length-offset]
         
     # init output arrays
         
@@ -177,13 +189,15 @@ def cross_proj(c1_spikes, c2_spikes, c1_mean_norm, c2_mean_norm, offset=0, norm=
     proj_2on2 = np.zeros((c2_spikes.shape[0]))
     
     # calculate cross-projections
+    norm = max(c1_norm, c2_norm)
+    
     for i in range(c1_spikes.shape[0]):
-        proj_1on1[i] = np.dot(c1_spikes[i].flatten(), c1_mean_norm.flatten())
-        proj_1on2[i] = np.dot(c1_spikes[i].flatten(), c2_mean_norm.flatten())
+        proj_1on1[i] = np.dot(c1_spikes[i].flatten(), c1_mean.flatten())/(c1_norm**2)
+        proj_1on2[i] = np.dot(c1_spikes[i].flatten(), c2_mean.flatten())/(norm**2)
         
     for i in range(c2_spikes.shape[0]):
-        proj_2on1[i] = np.dot(c2_spikes[i].flatten(), c1_mean_norm.flatten())
-        proj_2on2[i] = np.dot(c2_spikes[i].flatten(), c2_mean_norm.flatten())
+        proj_2on1[i] = np.dot(c2_spikes[i].flatten(), c1_mean.flatten())/(norm**2)
+        proj_2on2[i] = np.dot(c2_spikes[i].flatten(), c2_mean.flatten())/(c2_norm**2)
         
     return proj_1on1, proj_2on1, proj_1on2, proj_2on2
 

burst_detector/stages.py
@@ -31,7 +31,7 @@ def calc_mean_sim(data, times, clusters, counts, n_clust, labels, mean_wf, param
                 mean_wf[i,:,:] = np.nanmean(spikes, axis=0)
     
     # calculate mean similarity
-    mean_sim, offset, wf_means_norm = bd.wf_means_similarity(
+    mean_sim, wf_norms, offset = bd.wf_means_similarity(
         mean_wf, 
         jitter=params['jitter'],
         jitter_amt=params['jitter_amt']
@@ -47,10 +47,10 @@ def calc_mean_sim(data, times, clusters, counts, n_clust, labels, mean_wf, param
                         pass_ms[c1,c2] = True
                         pass_ms[c2,c1] = True
 
-    return mean_sim, offset, wf_means_norm, mean_wf, pass_ms
+    return mean_sim, offset, wf_norms, mean_wf, pass_ms
 
 
-def calc_cross_sim(spikes, wf_means_norm, offset, pass_ms, n_clust):
+def calc_cross_sim(spikes, offset, mean_wf, wf_norms, pass_ms, n_clust):
     wass_d = np.ones_like(offset, dtype='float64')
     num = 0
     
@@ -69,8 +69,10 @@ def calc_cross_sim(spikes, wf_means_norm, offset, pass_ms, n_clust):
                 proj_1on1, proj_2on1, proj_1on2, proj_2on2 = bd.cross_proj(
                     sp_1,
                     sp_2,
-                    wf_means_norm[c1],
-                    wf_means_norm[c2],
+                    mean_wf[c1],
+                    mean_wf[c2],
+                    wf_norms[c1],
+                    wf_norms[c2],
                     offset=offset[c1, c2]
                 )
                 
@@ -214,7 +216,7 @@ def recalc_ref_p(pairs, times, clusters, counts, params):
 
     return ref_pen, ref_per
 
-def merge_clusters(times, clusters, counts, n_clust, final_metric, cross_sim, xcorr_sig, ref_pen, mean_wf, channel_map, params):
+def merge_clusters(clusters, counts, mean_wf, final_metric, params):
     cl_max = clusters.max()
     
     # find channel with peak amplitude for each cluster
@@ -238,130 +240,57 @@ def merge_clusters(times, clusters, counts, n_clust, final_metric, cross_sim, xc
     new2old = {}
     old2new = {}
     new_ind = int(clusters.max() + 1)
-    rejected = []
-    orig_cl = cross_sim.shape[0] 
     
     # convert counts to array
-    temp = np.zeros(n_clust)
-    for i in range(n_clust):
+    temp = np.zeros(cl_max)
+    for i in range(cl_max):
         if i in counts:
             temp[i] = counts[i]
     counts = temp
 
     # merge logic
-    while pairs:
-        n = len(pairs)
-        for i in range(n):
-            pair = pairs.popleft(); c1 = int(pair[0]); c2 = int(pair[1])
+    for pair in pairs:
+        c1 = int(pair[0]); c2 = int(pair[1])
+
+        # both clusters are original
+        if c1 not in old2new and c2 not in old2new:
+            old2new[c1] = int(new_ind)
+            old2new[c2] = int(new_ind)
+            new2old[new_ind] = [c1, c2]
+            new_ind += 1
+
+        # one or more clusters has been merged already
+        else:
+            # get un-nested cluster list
+            cl1 = new2old[old2new[c1]] if c1 in old2new else [c1]
+            cl2 = new2old[old2new[c2]] if c2 in old2new else [c2]
+            if cl1 == cl2:
+                continue
+            cl_list = cl1 + cl2
             
-            if (c1 not in old2new) and (c2 not in old2new): # check if one of clusters has already been merged
-                # threshold metric if needed
-                if (c1 < orig_cl) and (c2 < orig_cl):
-                    merge = True
-                else:
-                    metric = np.sqrt(cross_sim[c1,c2]*cross_sim[c2,c1]) + params["xcorr_coeff"]*xcorr_sig[c1,c2] \
-                    - params["ref_pen_coeff"]*ref_pen[c1,c2]
-                    merge = metric > params['final_thresh']
-                # perform merge
-                if merge:
-                    old2new[c1] = new_ind
-                    old2new[c2] = new_ind
-                    new2old[new_ind] = (c1, c2)
-                    new_ind += 1
-                else:
-                    rejected.append((c1,c2))
-            else:
-                # convert merged clusters ids to new ones
-                if c1 in old2new:
-                    c1 = old2new[c1]
-                if c2 in old2new:
-                    c2 = old2new[c2]
-                if c1 != c2:
-                    pairs.append((c1,c2))
+            # iterate through cluster pairs
+            merge = True
+            for i in range(len(cl_list)):
+                for j in range(i+1, len(cl_list)):
+                    i1 = cl_list[i]
+                    i2 = cl_list[j]
                     
-        # edge case id conversion
-        for i in range(len(pairs)):
-            c1 = pairs[i][0]
-            c2 = pairs[i][1]
-            if c1 in old2new:
-                c1 = old2new[c1]
-            if c2 in old2new:
-                c2 = old2new[c2]
-            pairs[i] = (c1,c2)
-            
-            
-        # update cross_sim with weighted avg
-        nc = counts / counts.max() # prescale to prevent overflow
-        temp = np.zeros((new_ind, new_ind))
-        old_n = cross_sim.shape[0]
-        temp[:old_n, :old_n] = cross_sim
-        cross_sim = temp
-        for i in range(old_n, new_ind): # new-old intersections
-            c1 = new2old[i][0]
-            c2 = new2old[i][1]
-            cross_sim[i,:old_n] = (nc[c1]*cross_sim[c1,:old_n] + nc[c2]*cross_sim[c2,:old_n])/(nc[c1]+nc[c2])
-            cross_sim[:old_n, i] = (nc[c1]*cross_sim[:old_n,c1] + nc[c2]*cross_sim[:old_n,c2])/(nc[c1]+nc[c2])
-            
-        for i in range(old_n, new_ind): # new-new intersections
-            c1 = new2old[i][0]
-            c2 = new2old[i][1]
-            for j in range(old_n+1, new_ind):
-                c3 = new2old[j][0]
-                c4 = new2old[j][1]
-                cross_sim[i,j] = nc[c1]*nc[c3]*cross_sim[c1,c3] + nc[c1]*nc[c4]*cross_sim[c1,c4] + \
-                nc[c2]*nc[c3]*cross_sim[c2,c3] + nc[c2]*nc[c4]*cross_sim[c2,c4]
-                cross_sim[i,j] /= ((nc[c1]+nc[c2])*(nc[c3]+nc[c4]))
+                    if final_metric[i1,i2] < params["final_thresh"]:
+                        merge = False
+
+            # do merge
+            if merge:
+                for i in range(len(cl_list)):
+                    old2new[cl_list[i]] = new_ind
+                new2old[new_ind] = cl_list
+                new_ind += 1
                 
-       # update xcorr_sig with weighted avges
-        temp = np.zeros((new_ind, new_ind))
-        old_n = xcorr_sig.shape[0]
-        temp[:old_n, :old_n] = xcorr_sig
-        xcorr_sig = temp
-        for i in range(old_n, new_ind): # new-old intersections
-            c1 = new2old[i][0]
-            c2 = new2old[i][1]
-            xcorr_sig[i,:old_n] = (nc[c1]*xcorr_sig[c1,:old_n] + nc[c2]*xcorr_sig[c2,:old_n])/(nc[c1]+nc[c2])
-            xcorr_sig[:old_n, i] = (nc[c1]*xcorr_sig[:old_n,c1] + nc[c2]*xcorr_sig[:old_n,c2])/(nc[c1]+nc[c2])
-            
-        for i in range(old_n, new_ind): # new-new intersections
-            c1 = new2old[i][0]
-            c2 = new2old[i][1]
-            for j in range(old_n+1, new_ind):
-                c3 = new2old[j][0]
-                c4 = new2old[j][1]
-                xcorr_sig[i,j] = nc[c1]*nc[c3]*xcorr_sig[c1,c3] + nc[c1]*nc[c4]*xcorr_sig[c1,c4] + \
-                nc[c2]*nc[c3]*xcorr_sig[c2,c3] + nc[c2]*nc[c4]*xcorr_sig[c2,c4]
-                xcorr_sig[i,j] /= ((nc[c1]+nc[c2])*(nc[c3]+nc[c4]))
-                
-        # update clusters, counts
-        for i in range(clusters.shape[0]):
-            if clusters[i] in old2new:
-                clusters[i] = old2new[clusters[i]]
-        
-        counts = bd.spikes_per_cluster(clusters)
-        n_clust = clusters.max() + 1
-        # convert counts to array
-        temp = np.zeros(n_clust)
-        for i in range(n_clust):
-            if i in counts:
-                temp[i] = counts[i]
-        counts = temp
-        
-        # recalculate ref_pen
-        rp, rr = recalc_ref_p(pairs, times, clusters, counts, params)
-        temp = np.zeros((new_ind, new_ind))
-        old_n = ref_pen.shape[0]
-        temp[:old_n, :old_n] = ref_pen
-        ref_pen = temp
-        
-        for i in range(len(pairs)):
-            ref_pen[pairs[i][0], pairs[i][1]] = rp[i]
-        
-    # build lists to return
-    stage_mets = [cross_sim, xcorr_sig, ref_pen]
-    merge_lists = [old2new, new2old, rejected]
+    # remove intermediate clusters
+    for key in list(set(new2old.keys())):
+        if key not in list(set(old2new.values())):
+            del new2old[key]
     
-    return clusters, counts, merge_lists, stage_mets
+    return old2new, new2old
 
 
 # Multithreading function definitions